762bbcb18c1da8c80ecc284638af54d7e2ed9d37
Felix Jodoin <felixlinux@gmail.com>
2013-07-16 00:53:38 -0700

    Truck#slot_for_weekday

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -5,11 +5,15 @@ class Truck < ActiveRecord::Base
 
   # Returns all trucks with slots available on the given day
   def self.available_on day
-    joins(:slots).merge(Slot.available_on(day))
+    joins(:slots).merge(Slot.available_on(day)).includes(:slots)
   end
 
   def available_on? day
     slots.where(weekday: day).any?
+  end
+
+  def slot_for_day day
+    slots.find_by_weekday day
   end
 
 end
