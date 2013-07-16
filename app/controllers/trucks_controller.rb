9a9c93d43e4d0231f30c2a8c891840eecc4e4b45
Felix Jodoin <felixlinux@gmail.com>
2013-07-15 23:05:26 -0700

    current_weekday helper

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -1,6 +1,8 @@
 class TrucksController < ApplicationController
+  include ApplicationHelper
+
   def index
-    @trucks = Truck.available_today
+    @trucks = Truck.available_on current_weekday
   end
 
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
+require 'junk_drawer/weekdays'
+
 module ApplicationHelper
+
+  def current_weekday
+    JunkDrawer::Weekdays.current_weekday
+  end
+
 end
