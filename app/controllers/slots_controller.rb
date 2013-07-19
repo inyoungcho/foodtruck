d6b070e0a8753fd8f034c98428a5f6bf6b516fef
Felix Jodoin <felixlinux@gmail.com>
2013-07-18 22:56:44 -0700

    slots controller can now filter by query params

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -2,7 +2,7 @@ class SlotsController < ApplicationController
   before_filter :load_services
 
   def index
-    @slots = @fetch_slots_service.call
+    @slots = @fetch_slots_service.call params.reject { |_, v| v.blank? }
     respond_with @slots
   end
 
