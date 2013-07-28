d55253dab46dfda9b5def2238daa50c9ce6d4d26
Felix Jodoin <felixlinux@gmail.com>
2013-07-27 17:23:07 -0700

    dead code removal

--- app/assets/javascripts/models/slot.js
+++ app/assets/javascripts/models/slot.js
@@ -6,7 +6,3 @@ App.Slot = DS.Model.extend({
 
   truck: DS.belongsTo('App.Truck')
 });
-
-// App.Slot.FIXTURES = [
-// { "id":2,"truck_id":2,weekday:"Wednesday","address":"2121 6th Ave","neighborhood":"South Lake Union","period":"11AM-1PM","created_at":"2013-07-18T04:55:56.765Z","updated_at":"2013-07-18T04:55:56.765Z" }
-// ];
