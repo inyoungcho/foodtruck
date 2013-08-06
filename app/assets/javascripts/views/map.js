f6ad1111821768373217ff50897f7e395e7db526
Felix Jodoin <felixlinux@gmail.com>
2013-08-05 21:45:28 -0700

    fix maps zoom bounds

--- app/assets/javascripts/views/map.js
+++ app/assets/javascripts/views/map.js
@@ -1,4 +1,12 @@
-var mapSlot = function(slot, map, geocoder) {
+var fitBoundsAndKeepZoom = function(map, bounds) {
+  map.fitBounds(bounds);
+  // this seems so stupid
+  google.maps.event.addListenerOnce(map, 'bounds_changed', function(e) {
+    this.setZoom(16);
+  });
+}
+
+var mapSlot = function(slot, map, geocoder, bounds) {
   geocoder.geocode( { 'address': slot.get('address') + ", Seattle, WA" }, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       map.setCenter(results[0].geometry.location);
@@ -7,41 +15,50 @@ var mapSlot = function(slot, map, geocoder) {
           map: map,
           position: point
       });
-      return point;
+
+      // Update the bounds as a new slot is mapped
+      bounds.extend(point);
+      fitBoundsAndKeepZoom(map, bounds);
     }
   });
 }
 
-var mapAllSlots = function(slots, map, geocoder) {
-  var overallBounds = new google.maps.LatLngBounds();
-
+var mapAllSlots = function(slots, map, geocoder, bounds) {
   slots.forEach(function(slot) {
-    overallBounds.extend(mapSlot(slot, map, geocoder));
+    mapSlot(slot, map, geocoder, bounds);
   });
-
-  map.fitBounds(overallBounds);
 }
 
 App.TruckMapView = Ember.View.extend({
   classNames: ['map-canvas'],
   map: null,
   geocoder: null,
-  // Map all slots by default
-  didInsertElement: function() {
+  bounds: null,
+  selectedSlotBinding: 'controller.selectedSlot',
+
+  initializeMap: function() {
     var mapOptions = {
-      center: new google.maps.LatLng(-34.397, 150.644),
-      zoom: 16,
       disableDefaultUI: true,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
+
     var map = new google.maps.Map(this.$().get(0), mapOptions);
     var geocoder = new google.maps.Geocoder();
+    var bounds = new google.maps.LatLngBounds();
+
     this.set('map', map);
     this.set('geocoder', geocoder);
-    var slots = this.get('controller').get('model').get('slots');
-    mapAllSlots(slots, map, geocoder);
+    this.set('bounds', bounds);
   },
-  reRenderMap: function() {
 
-  }
+  didInsertElement: function() {
+    this.initializeMap();
+    // Map all slots by default
+    var slots = this.get('controller').get('model').get('slots');
+    mapAllSlots(slots, this.get('map'), this.get('geocoder'), this.get('bounds'));
+  },
+  
+  reRenderMap: function() {
+    alert("rerendering");
+  }.observes('selectedSlot')
 });
