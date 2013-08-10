c786a5b349fc5433abae7f6310faabbde87f1653
Felix Jodoin <felixlinux@gmail.com>
2013-08-10 16:11:04 -0700

    slots should be destroyed when the owning truck is destroyed

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,7 +1,7 @@
 class Truck < ActiveRecord::Base
   validates_presence_of :name, :kind, :description
   validates_uniqueness_of :name
-  has_many :slots
+  has_many :slots, dependent: :destroy
 
   # Returns all trucks with slots available on the given day
   def self.available_on day
