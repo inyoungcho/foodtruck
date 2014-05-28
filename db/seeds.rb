239c5a56b19bb42bb2b586a0fbeadc9236ffefce
Felix Jodoin <felix@fjstudios.net>
2014-05-28 15:39:58 -0700

    update seed corrections

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# Truck.select(:kind).distinct.to_a.map { |t| t.kind }.sort
+# Slot.select(:neighborhood).distinct.to_a.map { |s| s.neighborhood }.sort
+
 scraper = Scrape::Scraper.new
 scraper.import!
+
 # Correct some bad data
-Slot.find_by_neighborhood('5 pm. – 8 pm.').update_attributes(neighborhood: 'Columbia City')
-Slot.find_by_neighborhood('Downtown seattle').update_attributes(neighborhood: 'Downtown Seattle')
+Slot.find_by_neighborhood('Downtown seattle').try(:update_attributes, neighborhood: 'Downtown Seattle')
 Slot.find_by_neighborhood('SLU').try(:destroy)
+Slot.find_by_neighborhood('Downtown Seasttle').try(:update_attributes, neighborhood: 'Downtown Seattle')
+Slot.find_by_neighborhood('South lake Union').try(:update_attributes, neighborhood: 'South Lake Union')
+Slot.find_by_neighborhood('Queen ANne').try(:update_attributes, neighborhood: 'Queen Anne')
+Slot.find_by_neighborhood('U District').try(:update_attributes, neighborhood: 'University District')
+Slot.find_by_neighborhood('Remond').try(:update_attributes, neighborhood: 'Redmond')
 Truck.find_by_kind('comfort food').try(:destroy)
-Truck.find_by_kind('Comfort Food – Coming Soon!').update_attributes(kind: 'Comfort Food')
-Truck.find_by_kind('Southern – Coming Soon!').update_attributes(kind: 'Southern')
+Truck.find_by_kind('Indonedian').try(:update_attributes, kind: 'Indonesian')
 Truck.find_by_kind('Sweets (closed for winter)').try(:destroy)
 Truck.find_by_kind('Korean (closed for winter)').try(:destroy)
 Truck.find_by_kind('Hot Dogs (closed for winter)').try(:destroy)
